3c1688f0309b850928ea08216d381fc4bc3f8f1e
KostyaMerenkov <KonstantinLikes@gmail.com>
2021-10-23 18:46:08 +1000

    tests done

--- app/src/main/java/com/geekbrains/tests/view/search/MainActivity.kt
+++ app/src/main/java/com/geekbrains/tests/view/search/MainActivity.kt
@@ -49,23 +49,25 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
     private fun setQueryListener() {
         searchEditText.setOnEditorActionListener(OnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                val query = searchEditText.text.toString()
-                if (query.isNotBlank()) {
-                    presenter.searchGitHub(query)
-                    return@OnEditorActionListener true
-                } else {
-                    Toast.makeText(
-                        this@MainActivity,
-                        getString(R.string.enter_search_word),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@OnEditorActionListener false
-                }
+                searchAction()
             }
             false
         })
+        searchButton.setOnClickListener {
+            searchAction()
+        }
     }
 
+    private fun searchAction(): Boolean {
+        val query = searchEditText.text.toString()
+        return if (query.isNotBlank()) {
+            presenter.searchGitHub(query)
+            true
+        } else {
+            Toast.makeText(this@MainActivity, getString(R.string.enter_search_word), Toast.LENGTH_SHORT).show()
+            false
+        }
+    }
     private fun createRepository(): RepositoryContract {
         return if (BuildConfig.TYPE == FAKE) {
             FakeGitHubRepository()
